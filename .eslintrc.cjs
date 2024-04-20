37a1b01be9e9de94c1a7bcf8b2d6a7f8616d3888
Polaris-algedi <karchalmohammed@gmail.com>
2024-04-20 23:01:03 +0100

    Add Vite configuration file, main.jsx, index.html, App.css, README.md, package.json, App.jsx, .eslintrc.cjs, and index.css

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+  plugins: ["react", "import", "jsx-a11y", "react-refresh"],
+  rules: {
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
