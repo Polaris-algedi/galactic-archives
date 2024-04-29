f83684cc75a91ec35dcfdf7fa1581a0c3ca0a369
Polaris-algedi <karchalmohammed@gmail.com>
2024-04-29 20:10:50 +0100

    Refactor App component to add new features and update imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 //import Header from "./Header";
 import Hero from "./Hero";
+import AboutUs from "./AboutUs";
+import Features from "./Fearure2";
+
+import FeatureDetails from "./FeatureDetails";
 
 export default function App() {
   return (
     <>
       {/* {<Header />} */}
-      {<Hero />}
+      <Hero />
+      <Features />
+      <FeatureDetails />
+      <AboutUs />
     </>
   );
 }
