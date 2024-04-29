50e4c4f1d28df90219b81625732a54e7efc535ef
Polaris-algedi <karchalmohammed@gmail.com>
2024-04-29 20:14:06 +0100

    Refactor import statement in App.jsx to use AllFeatures instead of Fearure2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //import Header from "./Header";
 import Hero from "./Hero";
 import AboutUs from "./AboutUs";
-import Features from "./Fearure2";
+import Features from "./AllFeatures";
 
 import FeatureDetails from "./FeatureDetails";
 
