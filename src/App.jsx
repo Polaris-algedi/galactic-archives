de0a4ac347db839fc6662533cf623cb5a89b526d
Polaris-algedi <karchalmohammed@gmail.com>
2024-05-01 05:58:13 +0100

    Add LandingPageFooter component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,44 @@
 import Hero from "./Hero";
 import AboutUs from "./AboutUs";
 import Features from "./AllFeatures";
+import LandingPageFooter from "./LandingPageFooter";
 
 import FeatureDetails from "./FeatureDetails";
 
+//import { useState, useEffect } from "react";
+
 export default function App() {
+  /*   const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }; */
+
   return (
     <>
       {/* {<Header />} */}
+
       <Hero />
       <Features />
       <FeatureDetails />
       <AboutUs />
+      <LandingPageFooter />
     </>
   );
 }
