444e884b2840e6f0c810ac0b30adccdeec3350fe
Polaris-algedi <karchalmohammed@gmail.com>
2024-05-09 03:35:20 +0100

    refactor: Add LandingPage component to consolidate page sections

---
+++ src/LandingPage.jsx
@@ -0,0 +1,16 @@
+import Hero from "./Hero";
+import AboutUs from "./AboutUs";
+import Features from "./AllFeatures";
+
+import FeatureDetails from "./FeatureDetails";
+
+export default function LandingPage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FeatureDetails />
+      <AboutUs />
+    </>
+  );
+}
