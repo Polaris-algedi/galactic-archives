13f8ab46926cadb8f4a27f1e4ff7d329444c0fab
Polaris-algedi <karchalmohammed@gmail.com>
2024-05-24 05:09:37 +0100

    refactor: Update ImageSlider component to use dynamic images and improve slide selection feature

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 
 export default function ImageSlider({ images }) {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [selectedSlide, setSelectedSlide] = useState(0);
 
   const prevSlide = () => {
     const isFirstSlide = currentSlide === 0;
@@ -33,27 +32,27 @@ export default function ImageSlider({ images }) {
           <ChevronDoubleLeftIcon className="h-6 w-6 text-red-500 lg:h-8 lg:w-8" />
         </button>
         <div className="mx-auto flex w-4/5 overflow-hidden rounded-md">
-          {images.map((slide, index) => (
+          {images.map((image, index) => (
             <div
               key={index}
               className="duration-800 w-1/4 flex-shrink-0  p-5 transition ease-out"
               style={{ transform: `translateX(${currentSlide * -100}%)` }}
             >
               <button
-                className={`relative overflow-hidden rounded-lg shadow-lg transition duration-300 ease-out hover:scale-105 active:scale-100 ${selectedSlide === index ? "scale-105" : " "}`}
-                onClick={() => setSelectedSlide(index)}
+                className={`relative overflow-hidden rounded-lg shadow-lg transition duration-300 ease-out hover:scale-105 active:scale-100 ${image.selected.episode_id - 1 === index ? "scale-105" : " "}`}
+                onClick={() => image.onClick()}
               >
                 <img
-                  src={slide.img}
+                  src={image.img}
                   alt="Slide"
                   className="h-96 rounded-lg object-cover"
                 />
                 <div className="absolute inset-0 rounded-lg bg-black bg-opacity-30 hover:bg-opacity-5">
-                  <div className="absolute inset-x-0 bottom-0 rounded-b-lg bg-black bg-opacity-30">
+                  {/* <div className="absolute inset-x-0 bottom-0 rounded-b-lg bg-black bg-opacity-30">
                     <h2 className="p-2 text-center text-3xl font-bold text-white">
-                      {slide.title}
+                      {image.title}
                     </h2>
-                  </div>
+                  </div> */}
                 </div>
               </button>
             </div>
