2df0773d99a8d791449ed542cb272b84d3b4e130
Polaris-algedi <karchalmohammed@gmail.com>
2024-06-03 17:24:55 +0100

    refactor: Update Card component to include style prop
    This commit updates the Card component to include a new prop called style. The style prop allows for custom styling of the Card component, providing more flexibility in the visual presentation of the component. This change improves the reusability and customization options of the Card component, enhancing the overall user experience.

--- src/ReusableComponents/Card.jsx
+++ src/ReusableComponents/Card.jsx
@@ -1,8 +1,14 @@
-export default function Card({ children, className, onClick = () => {} }) {
+export default function Card({
+  children,
+  className,
+  style,
+  onClick = () => {},
+}) {
   return (
     <button
       className={`relative overflow-hidden rounded-lg shadow-lg transition duration-300 ease-out hover:scale-105 active:scale-100 md:hover:scale-110 ${className}`}
       onClick={onClick}
+      style={style}
     >
       {children}
       <div className="absolute inset-0 rounded-lg bg-black/20 hover:bg-black/5" />
